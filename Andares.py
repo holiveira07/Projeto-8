8a2ca927cc24b8e0d53df4b534a67f5d2321ebc7
holiveira07 <oliveirasalgadolh@gmail.com>
2023-03-11 15:15:38 -0300

    Exercicio 9

--- Andares.py
+++ Andares.py
@@ -1,5 +1,5 @@
 A = "Andar"
 for i in range(20, 1, -1):
-  if i == 13:
-    continue
-  print(i, "Andar")
+     if i == 13:  
+      continue
+print(i, "Andar")
--- Digite seu andar.py
+++ Digite seu andar.py
@@ -1,5 +1,5 @@
 r = "S"
 while r == "S":
-  n = int(input("Digite seu andar: "))
-  r = str(input("Ir para o seu andar? [S/N]"))
+    n = int(input("Digite seu andar: "))
+    r = str(input("Ir para o seu andar? [S/N]"))
 print("ok,", "indo para o seu andar !")
